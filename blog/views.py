7a4d9a7e85c4210b58c6a7a0c740a963514fa898
PJH6029 <top231902@naver.com>
2021-01-08 14:26:58 +0900

    CBV 이용, 블로그 포스트 상세 페이지 제작

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
@@ -8,6 +7,8 @@ class PostList(ListView):
     model = Post
     ordering = '-pk'
 
+class PostDetail(DetailView):
+    model = Post
 
 '''
 def index(request):
@@ -20,14 +21,15 @@ def index(request):
             'posts': posts,
         }
     )
-'''
+
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
 
     return render(
         request,
-        'blog/single_post_page.html',
+        'blog/post_detail.html',
         {
             'post': post,
         }
     )
+'''
