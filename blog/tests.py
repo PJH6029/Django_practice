334132c2fc96a55a1dc80c784e8c4322f332acb7
PJH6029 <top231902@naver.com>
2021-06-30 00:26:35 +0900

    댓글을 포스트 상세 페이지에 나타내기

--- blog/tests.py
+++ blog/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
 from django.contrib.auth.models import User
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 
 # Create your tests here.
 
@@ -52,6 +52,12 @@ class TestView(TestCase):
         # print(self.post_001.tags.exists())
         # print(self.post_002.tags.exists())
         # print(self.post_uncategorized.tags.exists())
+
+        self.comment_001 = Comment.objects.create(
+            post=self.post_001,
+            author=self.user_obama,
+            content='첫 번째 댓글입니다. ',
+        )
 
 
         self.posts = [self.post_001, self.post_002]
@@ -182,6 +188,12 @@ class TestView(TestCase):
 
         self.assertIn(self.user_trump.username.upper(), post_area.text)
         self.assertIn(self.post_001.content, post_area.text)
+
+        # comment area
+        comments_area = soup.find('div', id='comment-area')
+        comment_001_area = comments_area.find('div', id='comment-1')
+        self.assertIn(self.comment_001.author.username, comment_001_area.text)
+        self.assertIn(self.comment_001.content, comment_001_area.text)
 
     def test_create_post(self):
         # log in x: status_code != 200
