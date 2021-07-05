c36719dd3419ca0b759f3c85af48ca9c05de620e
PJH6029 <top231902@naver.com>
2021-07-05 19:47:03 +0900

    avatar

--- blog/models.py
+++ blog/models.py
@@ -73,6 +73,12 @@ class Comment(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/205/85a7102a015de50d/svg/{self.author.email}/'
+
     def __str__(self):
         return f'{self.author}::{self.content}'
 
